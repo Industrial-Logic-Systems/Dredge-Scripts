dfa6773abb696ff54f57e829576f12df5e925204
Luke Eltiste <luke343279@gmail.com>
2021-09-24 14:50:35 -0700

    update freeboard handeling of NonEff events

--- freeboardUpdater.py
+++ freeboardUpdater.py
@@ -1,37 +1,46 @@
 import dweepy
 import logging
 
-# Function codes and their meanings
 function_codes = {
+    "CCSH": "Clearing Cutter/Suction Head",
+    "CESS": "Cessation",
+    "CLPJ": "Change Cut/Area",
+    "COLL": "Collisions",
+    "CPPL": "Clearing Pump & Pipelines",
+    "FBD": "Fire Drills",
+    "HPL": "Handling Pipelines",
+    "HSL": "Handling Swing Lines",
+    "LDNE": "Loss Due to Natural Elements",
+    "LDPV": "Traffic / Loss Due to Passing Vessel",
+    "LNL": "Transferring Plant Between Works / Transfer to New Location",
+    "MAJ": "Major Repairs",
+    "MISC": "Miscellaneous",
+    "MSC": "Miscellaneous / Non-Pay",
+    "OR": "Minor Operating Repairs",
+    "OSS": "Off Shift / Saturdays",
+    "PREP": "Preparation & Making Up Tow",
+    "SBT": "Stand By Time (As Directed)",
+    "SH": "Sundays & Holidays",
+    "SLSW": "Shore Line / Shore Work",
+    "TFS": "Taking On Fuel & Supplies",
+    "TFWA": "To/From Wharf/Anchorage",
+    "WAP": "Waiting for Attendant Plant",
+    "WFB": "Waiting For Booster",
+    "WFS": "Waiting for Scows",
+}
+# Function codes and their meanings
+function_codes_depreciated = {
     "AGV": "Assisting Grounded Vessels",
     "CCH": "Changing Cutterhead",
-    "CCSH": "Clear Cutter Suction",
-    "CLPJ": "Change Location Bar",
-    "COLL": "Collision",
-    "CPPL": "Clear Pump Pipeline",
     "CPR": "Change Impeller",
     "DR": "Dike Repair",
-    "FBD": "Fire Boat Drills",
-    "HPL": "Handling Pipe Line",
-    "HSL": "Handling Swing Line",
     "HSP": "Handling Shore Pipe",
-    "LDNE": "Loss Due to Natural Elements",
-    "LDPV": "Loss Due to Passing Vessel",
-    "LNL": "Transfer to New Location",
-    "MISC": "Miscellaneous",
     "MOB": "Mobilization & Demobilization",
-    "MSC": "Miscellaneous/Non-pay",
     "OC": "Out of Commission",
-    "OR": "Operating Repairs",
     "P": "Preperation",
-    "PREP": "Preparation & Making Up Tow",
     "RPL": "Repair Pipeline",
     "SB": "Sounding & Buoying",
-    "SBT": "Stand-By Time as Directed",
-    "SH": "Sundays-Holidays",
-    "TFS": "Taking on Fuel & Supplies",
     "TOW": "Time on Tow",
-    "WAP": "Waiting Attendant Plant",
 }
 
 
@@ -44,12 +53,35 @@ def freeboard(name, data):
         for event in events:
             if "non_eff_event" in event:
                 logging.debug("Non-Eff Freeboard")
+
+                msgStart = event["non_eff_event"]["msg_start_time"]
+                msgEnd = event["non_eff_event"]["msg_end_time"]
                 function_code = event["non_eff_event"]["function_code"].strip()
-                message = function_codes[function_code]
+                comment = event["non_eff_event"]["comment"].strip()
+
+                if (
+                    function_code in function_codes_depreciated
+                    and function_code not in function_codes
+                ):
+                    logging.warning("Function code is Depreciated")
+                    message = function_codes_depreciated[function_code]
+                else:
+                    message = function_codes[function_code]
+
                 logging.debug(
                     f'{name + "_non_eff"}, code: {function_code}, message, {message}'
                 )
-                dweepy.dweet_for(name + "_non_eff", {"message": message})
+                dweepy.dweet_for(
+                    name + "_non_eff",
+                    {
+                        "msgStart": msgStart,
+                        "msgEnd": msgEnd,
+                        "function_code": function_code,
+                        "comment": comment,
+                        "message": message,
+                    },
+                )
+
     except Exception as e:
         logging.error("Freeboard failed to update")
         logging.debug(e, exc_info=True)
